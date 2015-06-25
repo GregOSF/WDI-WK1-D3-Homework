8bd60d739e207a6771bcb1f15ff6c99dab785a18
GregOSF <gregory.onza@gmail.com>
2015-06-24 17:39:49 -0700

    Added prompt

--- excercise1.js
+++ excercise1.js
@@ -1,12 +1,11 @@
-
-for (i=5; (i <= 5) && (i >= 0); i--) {
+var verses=prompt("How much beer is there?")
+for (i=verses; (i <= verses) && (i >= 0); i--) {
     if (i == 1) {
         console.log(i + " bottle of beer on the wall, " + i + " bottle of beer! Take one down and pass it around, " + (i) + " bottle of beer on the wall.")    
     } else if (i == 0) {
         i = "No more";
         console.log(i + " bottles of beer on the wall, " + i + " bottles of beer! Take one down and pass it around, " + (i) + " bottles of beer on the wall.")
     } 
-
     else { 
     console.log(i + " bottles of beer on the wall, " + i + " bottles of beer! Take one down and pass it around, " + (i) + " bottles of beer on the wall.")
     }
